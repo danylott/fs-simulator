79cdae8ee68ded9b070a3a6e9b9e04c5a91964ed
d.tuytuyshkin@gmail.com <d.tuytuyshkin@gmail.com>
2021-04-25 14:27:05 +0300

    added test example using junit4

---
+++ src/test/java/SimpleTests.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SimpleTests {
+    @Test
+    public void shouldCreateDefaultArrayTest() {
+        assertEquals(2 * 2, 4);
+    }
+}
