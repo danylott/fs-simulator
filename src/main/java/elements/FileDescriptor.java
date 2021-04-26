4f4475f06d96a01083d7ed1f5e46eba0432b70b6
tmyroniuk <tmyroniuk@gmail.com>
2021-04-26 11:09:02 +0300

    Disc elements and utility classes

---
+++ src/main/java/elements/FileDescriptor.java
@@ -0,0 +1,13 @@
+package elements;
+
+public class FileDescriptor extends SerializableElement {
+    @Override
+    public void serialize(byte[] data, int pos) {
+
+    }
+
+    @Override
+    public void deserialize(byte[] data, int pos) {
+
+    }
+}
